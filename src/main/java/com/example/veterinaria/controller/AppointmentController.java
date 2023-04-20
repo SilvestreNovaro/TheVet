52831bcba0e15db849790fb6e1a3bd67501bbc5e
ArchSofia <sofiadiazvaldez@gmail.com>
2023-04-20 13:46:15 -0300

    Added AppointmentController

---
+++ src/main/java/com/example/veterinaria/controller/AppointmentController.java
@@ -0,0 +1,61 @@
+package com.example.veterinaria.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.example.veterinaria.entity.Appointment;
+import com.example.veterinaria.service.AppointmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/appointments")
+public class AppointmentController {
+
+    @Autowired
+    private AppointmentService appointmentService;
+
+    @PostMapping("/create")
+    public ResponseEntity<Appointment> createAppointment(@RequestBody Appointment appointment) {
+        Appointment savedAppointment = appointmentService.createAppointment(appointment);
+        return new ResponseEntity<>(savedAppointment, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Appointment> getAppointmentById(@PathVariable Long id) {
+        Optional<Appointment> appointment = appointmentService.getAppointmentById(id);
+        if (appointment.isPresent()) {
+            return new ResponseEntity<>(appointment.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Appointment>> getAllAppointments() {
+        List<Appointment> appointments = appointmentService.getAllAppointments();
+        return new ResponseEntity<>(appointments, HttpStatus.OK);
+    }
+
+    @GetMapping("/customer/{customerId}")
+    public ResponseEntity<List<Appointment>> getAppointmentsByCustomerId(@PathVariable Long customerId) {
+        List<Appointment> appointments = appointmentService.getAppointmentsByCustomerId(customerId);
+        return new ResponseEntity<>(appointments, HttpStatus.OK);
+    }
+
+    @GetMapping("/pet/{petId}")
+    public ResponseEntity<List<Appointment>> getAppointmentsByPetId(@PathVariable Long petId) {
+        List<Appointment> appointments = appointmentService.getAppointmentsByPetId(petId);
+        return new ResponseEntity<>(appointments, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAppointment(@PathVariable Long id) {
+        appointmentService.deleteAppointment(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+}
+
