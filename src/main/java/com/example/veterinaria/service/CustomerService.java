ac85ee34d448b62cc56cbd4ccd41f3c435bf3e6a
ArchSofia <sofiadiazvaldez@gmail.com>
2023-04-20 13:06:18 -0300

    Added services

--- src/main/java/com/example/veterinaria/service/CustomerService.java
+++ src/main/java/com/example/veterinaria/service/CustomerService.java
@@ -1,4 +1,39 @@
 package com.example.veterinaria.service;
 
+import com.example.veterinaria.entity.Customer;
+import com.example.veterinaria.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class CustomerService {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    public Customer createCustomer(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    public Customer updateCustomer(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    public List<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
+
+    public Optional<Customer> getCustomerById(Long id) {
+        return customerRepository.findById(id);
+    }
+
+    public void deleteCustomer(Long id) {
+        customerRepository.deleteById(id);
+    }
+
+    // add any additional methods here
+
 }
+
