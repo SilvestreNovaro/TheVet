89b1c46831ed8d4493a32575dd38471136dc8428
SilvestreNovaro <silvestrenovarohueyo@gmail.com>
2023-06-24 22:58:22 -0300

    INSERTS

---
+++ petitions/INSERT_ROLES.sql
@@ -0,0 +1,3 @@
+INSERT INTO Role (role_name) VALUES ('Customer');
+INSERT INTO Role (role_name) VALUES ('Admin');
+INSERT INTO Role (role_name) VALUES ('Vet');
---
+++ petitions/INSERT_VET.sql
@@ -0,0 +1,9 @@
+SELECT * FROM veterinaria.vet;
+
+
+INSERT INTO Vet (email, license, name, phone, surname) VALUES ('vet1@example.com', '123456789', 'John', '987654321', 'Doe');
+INSERT INTO Vet (email, license, name, phone, surname) VALUES ('vet2@example.com', '987654321', 'Jane', '123456789', 'Smith');
+INSERT INTO Vet (email, license, name, phone, surname) VALUES ('vet3@example.com', '456789123', 'Michael', '456123789', 'Johnson');
+INSERT INTO Vet (email, license, name, phone, surname) VALUES ('vet4@example.com', '789123456', 'Emily', '789456123', 'Davis');
+INSERT INTO Vet (email, license, name, phone, surname) VALUES ('vet5@example.com', '321654987', 'David', '321789654', 'Taylor');
+INSERT INTO Vet (email, license, name, phone, surname) VALUES ('vet6@example.com', '654987321', 'Sarah', '654789123', 'Anderson');
