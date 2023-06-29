527d4176deaec7898f302fac8f9d8710fdd63a4c
SilvestreNovaro <silvestrenovarohueyo@gmail.com>
2023-06-29 14:08:12 -0300

    INSERT_VET_NEW_VALUES

--- petitions/INSERT_VET.sql
+++ petitions/INSERT_VET.sql
@@ -1,9 +1,23 @@
 SELECT * FROM veterinaria.vet;
 
 
-INSERT INTO Vet (email, license, name, phone, surname) VALUES ('vet1@example.com', '123456789', 'John', '987654321', 'Doe');
-INSERT INTO Vet (email, license, name, phone, surname) VALUES ('vet2@example.com', '987654321', 'Jane', '123456789', 'Smith');
-INSERT INTO Vet (email, license, name, phone, surname) VALUES ('vet3@example.com', '456789123', 'Michael', '456123789', 'Johnson');
-INSERT INTO Vet (email, license, name, phone, surname) VALUES ('vet4@example.com', '789123456', 'Emily', '789456123', 'Davis');
-INSERT INTO Vet (email, license, name, phone, surname) VALUES ('vet5@example.com', '321654987', 'David', '321789654', 'Taylor');
-INSERT INTO Vet (email, license, name, phone, surname) VALUES ('vet6@example.com', '654987321', 'Sarah', '654789123', 'Anderson');
+INSERT INTO Vet (email, image, license, name, specialty, surName)
+VALUES ('vet1@example.com', 'https://st.depositphotos.com/1001951/4411/i/600/depositphotos_44117761-stock-photo-young-positive-brunette-veterinary-woman.jpg', '123456', 'John', 'Dentistry', 'Smith');
+
+INSERT INTO Vet (email, image, license, name, specialty, surName)
+VALUES ('vet2@example.com', 'https://koosh.com.ar/wp-content/uploads/veterinario.jpg', '789012', 'Jane', 'Surgery', 'Johnson');
+
+INSERT INTO Vet (email, image, license, name, specialty, surName)
+VALUES ('vet3@example.com', 'https://www.shutterstock.com/image-photo/young-handsome-veterinarian-petting-noble-260nw-2285054237.jpg', '345678', 'Michael', 'Dermatology', 'Williams');
+
+INSERT INTO Vet (email, image, license, name, specialty, surName)
+VALUES ('vet4@example.com', 'https://www.portalveterinaria.com/upload/20210719103659pexels-tima-miroshnichenko-6235231_pv.jpg', '901234', 'Emily', 'Ophthalmology', 'Brown');
+
+INSERT INTO Vet (email, image, license, name, specialty, surName)
+VALUES ('vet5@example.com', 'https://www.unavets.com/lleida/wp-content/uploads/sites/5/2021/07/Team-Alberto.png', '567890', 'David', 'Cardiology', 'Taylor');
+
+INSERT INTO Vet (email, image, license, name, specialty, surName)
+VALUES ('vet6@example.com', 'https://i0.wp.com/enelveterinario.com/wp-content/uploads/2020/07/cardiología.jpg?ssl=1', '234567', 'Sarah', 'Neurology', 'Anderson');
+
+INSERT INTO Vet (email, image, license, name, specialty, surName)
+VALUES ('vet7@example.com', 'https://cdn-3.expansion.mx/dims4/default/c9e7a8f/2147483647/strip/true/crop/1253x836+0+0/resize/1800x1201!/format/webp/quality/80/?url=https%3A%2F%2Fcdn-3.expansion.mx%2F41%2F85%2Ff3cd6a95480d9005c1d5c76fda9a%2Fdia-del-veterinario-cuanto-ganan-mexico.jpg', '890123', 'Daniel', 'Radiology', 'Miller');
--- petitions/SELECT.sql
+++ petitions/SELECT.sql
@@ -6,3 +6,4 @@ SELECT * FROM pet
 SELECT * FROM product
 SELECT * FROM role
 SELECT * FROM vet
+appointment_id
